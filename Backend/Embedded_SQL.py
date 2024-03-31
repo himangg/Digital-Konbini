f598dee7c2a43da313a2c339b3ebdc1283eaf42f
Nishil-Agarwal <132306904+Nishil-Agarwal@users.noreply.github.com>
2024-03-31 17:20:34 +0530

    Update Embedded_SQL.py

--- Backend/Embedded_SQL.py
+++ Backend/Embedded_SQL.py
@@ -405,13 +405,13 @@ def display_cart(customer_id):           #Returns cart of a customer
 
 def show_supplier_inventory(supplier_id):      #Returns all products and details that are supplied by a specific supplier
     '''
-    Returns a list of (Product name, price per unit, quantity left in stock, discount percentage offered by supplier himself) pairs.
+    Returns a list of (Product_ID,Product name, price per unit, quantity left in stock, discount percentage offered by supplier himself) pairs.
     Else returns error string
     '''
     connection=connectit()
     with connection.cursor() as cursor:
         try:
-            cursor.execute("select name,price*(100-discount_percentage)/100,quantity_remaining,discount_percentage from product where supplier_id=%s",supplier_id)
+            cursor.execute("select product_id,name,price*(100-discount_percentage)/100,quantity_remaining,discount_percentage from product where supplier_id=%s",supplier_id)
             connection.close()
             return cursor.fetchall()
         except Exception as e:
